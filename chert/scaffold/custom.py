8089f7b1c6e69ff26533d3eb6862e817a4da2f45
Mahmoud Hashemi <mahmoud@hatnote.com>
2015-05-25 01:31:43 -0700

    demonstrate exception handling behavior

--- chert/scaffold/custom.py
+++ chert/scaffold/custom.py
@@ -6,3 +6,9 @@ print ' - custom module loaded.'
 
 def chert_post_load(chert_obj):
     print ' - post_load hook: %s entries loaded' % len(chert_obj.entries)
+
+
+def chert_pre_audit(chert_obj):
+    # exceptions are automatically caught and logged
+    # just enable debug mode to see issues
+    raise ValueError('something went awry')
