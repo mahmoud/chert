f19fc21ebff840ca0d47ca2b61f0cd60c677c18e
Mahmoud Hashemi <mahmoud@hatnote.com>
2015-08-31 01:26:29 -0700

    adding working toc implementation based on html5lib

--- chert/toc.py
+++ chert/toc.py
@@ -1,12 +1,11 @@
+# -*- coding: utf-8 -*-
+"""Add a Table of Contents to any HTML via cElementTree and friends.
 
-"""
-Add a Table of Contents to any HTML via cElementTree and friends.
-
-Partially based on an extraction from python-markdown.
+Largely based on an extraction from python-markdown, but can be run on
+bare HTML (not a python-markdown-based ElementTree).
 """
 import re
-#from xml.etree import Element, ElementTree as ET
-from HTMLParser import HTMLParser
+from xml.etree import cElementTree as ET
 
 import html5lib
 from boltons.strutils import slugify
@@ -19,10 +18,29 @@ def to_unicode(obj):
         return unicode(obj, encoding='utf8')
 
 
+def add_toc(html, marker='[TOC]', title='Contents', base_header_level=1):
+    """Adds a table of contents div where *marker* can be found within p
+    tags on its own. Turns headings into links and optionally adjusts
+    their level to be suitable for inclusion in a larger document.
+
+    Input can be text or utf-8 bytes, but the return is text (aka a
+    unicode object).
+
+    Intended to be used on the "content" (part below the post title)
+    of the HTML layout of an entry.
+    """
+    tocifier = TOCifier(html=html,
+                        marker=marker,
+                        title=title,
+                        base_header_level=base_header_level)
+    tocifier.process()
+    html_text = tocifier.get_html_text()
+    return html_text
+
+
 class TOCifier(object):
     id_count_re = re.compile(r'^(.*)_([0-9]+)$')
     header_re = re.compile("[Hh][123456]")
-    slugify = slugify
 
     def __init__(self, html, marker, title, base_header_level=1):
         self.raw_html = html
@@ -30,13 +48,14 @@ class TOCifier(object):
         self.title = title
         self.html = to_unicode(html)
         self.base_header_level = base_header_level
+        self.slugify = slugify
 
-    def run(self):
-        self.root = html5lib.parse(self.html)
-        self.used_ids = set()
+    def process(self):
+        self.root = html5lib.parse(self.html, namespaceHTMLElements=False)
+        self.used_id_set = set()
 
-        header_tokens = []
-        for el in self.root:
+        h_tokens = []
+        for el in self.root.iter():
             if not isinstance(el.tag, basestring):
                 continue
             h_match = self.header_re.match(el.tag)
@@ -50,15 +69,79 @@ class TOCifier(object):
                 slug = self.slugify(text)
                 el.attrib['id'] = self.get_unique_id(slug)
 
-            header_tokens.append({'level': int(el.tag[-1]),
-                                  'id': el.attrib['id'],
-                                  'text': text})
+            h_tokens.append({'level': int(el.tag[-1]),
+                             'id': el.attrib['id'],
+                             'text': text})
             self.anchorize_header(el)
-        import pdb;pdb.set_trace()
+        h_token_tree = nest_h_tokens(h_tokens)
+        toc_div_el = self.build_toc_div(h_token_tree)
+        if self.marker:
+            self.replace_marker(toc_div_el)
         return
 
+    def get_html_text(self):
+        serializer = html5lib.serializer.HTMLSerializer()
+        walker = html5lib.getTreeWalker('etree')
+        stream = serializer.serialize(walker(self.root))
+        return u''.join(stream)
+
+    def replace_marker(self, elem):
+        ''' Replace marker with elem. '''
+        pc_pairs = ((parent, child)
+                    for parent in self.root.iter()
+                    for child in parent)
+        for (p, c) in pc_pairs:
+            if not isinstance(c.tag, basestring):
+                continue
+            if c.tag in ('pre', 'code'):
+                continue
+            if self.header_re.match(c.tag):
+                continue
+            text = ''.join(c.itertext()).strip()
+            if not text:
+                continue
+
+            # To keep the output from screwing up the
+            # validation by putting a <div> inside of a <p>
+            # we actually replace the <p> in its entirety.
+            # We do not allow the marker inside a header as that
+            # would causes an enless loop of placing a new TOC
+            # inside previously generated TOC.
+            if c.text and c.text.strip() == self.marker:
+                for i in range(len(p)):
+                    if p[i] == c:
+                        p[i] = elem
+                        break
+        return
+
+    def build_toc_div(self, h_token_tree):
+        """ Return a string div given a toc list. """
+        div_el = ET.Element("div")
+        div_el.attrib["class"] = "toc"
+
+        # Add title to the div
+        if self.title:
+            title_el = ET.SubElement(div_el, "span")
+            title_el.attrib["class"] = "toctitle"
+            title_el.text = self.title
+
+        def build_etree_ul(toc_list, parent):
+            ul = ET.SubElement(parent, "ul")
+            for item in toc_list:
+                # List item link, to be inserted into the toc div
+                li = ET.SubElement(ul, "li")
+                link = ET.SubElement(li, "a")
+                link.text = item.get('name', '')
+                link.attrib["href"] = '#' + item.get('id', '')
+                if item['children']:
+                    build_etree_ul(item['children'], li)
+            return ul
+
+        build_etree_ul(h_token_tree, div_el)
+        return div_el
+
     def anchorize_header(self, header_el):
-        anchor_el = Element("a")
+        anchor_el = ET.Element("a")
         anchor_el.text = header_el.text
         anchor_el.attrib["href"] = "#" + header_el.attrib['id']
         anchor_el.attrib["class"] = "toclink"
@@ -76,8 +159,9 @@ class TOCifier(object):
             level = 6
         header_el.tag = 'h%d' % level
 
-    def get_unique_id(self, id_str, used_id_set):
+    def get_unique_id(self, id_str):
         """ Ensure id is unique in set of ids. Append '_1', '_2'... if not """
+        used_id_set = self.used_id_set
         while id_str and id_str in used_id_set:
             match = self.id_count_re.match(id_str)
             if match:
@@ -89,82 +173,75 @@ class TOCifier(object):
         return id_str
 
 
-class TOCHTMLProcessor(HTMLParser):
-    def __init__(self, tocifier):
-        self.tocifier = tocifier
-        self.cur_h_text = ''
-        self.cur_h_level = 0
-        self.cur_h_startpos = None
-        self.cur_h_endpos = None
-        self.cur_h_id = None
-        self.h_tokens = []
-        HTMLParser.__init__(self)
+def nest_h_tokens(h_tokens):
+    """Given an unsorted list with errors and skips, return a nested one.
+    [{'level': 1}, {'level': 2}]
+    =>
+    [{'level': 1, 'children': [{'level': 2, 'children': []}]}]
 
-    def handle_starttag(self, tag, attrs):
-        h_match = self.tocifier.header_re.match(tag)
-        if not h_match:
-            return
-        if self.cur_h_offset:
-            raise ValueError('h tags do not nest')
+    A wrong list is also converted:
+    [{'level': 2}, {'level': 1}]
+    =>
+    [{'level': 2, 'children': []}, {'level': 1, 'children': []}]
+    """
+    toc_list = list(h_tokens)
+    ordered_list = []
+    if not toc_list:
+        return ordered_list
 
+    # Initialize everything by processing the first entry
+    last = toc_list.pop(0)
+    last['children'] = []
+    levels = [last['level']]
+    ordered_list.append(last)
+    parents = []
 
-    def handle_data(self, data):
-        pass
+    # Walk the rest nesting the entries properly
+    while toc_list:
+        t = toc_list.pop(0)
+        current_level = t['level']
+        t['children'] = []
 
-    def handle_endtag(self, tag):
-        pass
+        # Reduce depth if current level < last item's level
+        if current_level < levels[-1]:
+            # Pop last level since we know we are less than it
+            levels.pop()
 
+            # Pop parents and levels we are less than or equal to
+            to_pop = 0
+            for p in reversed(parents):
+                if current_level <= p['level']:
+                    to_pop += 1
+                else:  # pragma: no cover
+                    break
+            if to_pop:
+                levels = levels[:-to_pop]
+                parents = parents[:-to_pop]
 
-def get_used_ids(html):
-    ret = set()
-    id_parser = HTMLParser()
-    id_parser.handle_starttag = lambda _, attrs: ret.add(dict(attrs).get('id'))
-    id_parser.feed(html)
-    id_parser.close()
-    ret.remove(None)
-    return ret
+            # Note current level as last
+            levels.append(current_level)
 
+        # Level is the same, so append to
+        # the current parent (if available)
+        if current_level == levels[-1]:
+            (parents[-1]['children'] if parents
+             else ordered_list).append(t)
 
-class TOCifierNo(object):
-    id_count_re = re.compile(r'^(.*)_([0-9]+)$')
-    header_re = re.compile("[Hh][123456]")
-    slugify = slugify
+        # Current level is > last item's level,
+        # So make last item a parent and append current as child
+        else:
+            last['children'].append(t)
+            parents.append(last)
+            levels.append(current_level)
+        last = t
 
-    def __init__(self, html, marker, title, base_header_level=1):
-        self.raw_html = html
-        self.marker = marker
-        self.title = title
-        self.html = to_unicode(html)
-        self.base_header_level = base_header_level
-
-    def run(self):
-        self.used_ids = get_used_ids(self.html)
-        # build tokens
-        # text replace
-
-    def get_unique_id(self, id_str, used_id_set):
-        """ Ensure id is unique in set of ids. Append '_1', '_2'... if not """
-        while id_str and id_str in used_id_set:
-            match = self.id_count_re.match(id_str)
-            if match:
-                new_num = int(match.group(2)) + 1
-                id_str = '%s_%d' % (match.group(1), new_num)
-            else:
-                id_str = '%s_%d' % (id_str, 1)
-            used_id_set.add(id_str)
-        return id_str
+    return ordered_list
 
 
 def main():
-    html = open('/home/mahmoud/projects/sedimental/site/build_your_own_topic_bot.html').read()
-    tocifier = TOCifier(html, '', '')
-    tocifier.run()
-    #thp = TOCHTMLProcessor(tocifier)
-    #thp.feed(html)
-    #thp.close()
-    #print
-    #print thp.id_set
-    # ''.join(html5lib.serializer.HTMLSerializer().serialize(html5lib.getTreeWalker('etree')(self.root)))
+    raw_html = open('/home/mahmoud/projects/sedimental/site/podcasts.html').read()
+    html = raw_html[raw_html.index('<body>'):raw_html.index('</body>') + 7]
+    print add_toc(html).encode('utf-8')
     return
 
 
